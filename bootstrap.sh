6f9cd409736546a38c1f627a18ec9198a676cd69
Ch4s3r <lamprecht.patrick1@gmail.com>
2024-05-19 17:49:16 +0200

    chore: Move argocd applicationset and update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,10 @@ set -x KUBECONFIG ~/.kube/macmini
 
 k3d cluster create -i latest
 kubectl annotate storageclass local-path defaultVolumeType=local
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
 velero install \
         --use-node-agent \
         --default-volumes-to-fs-backup \
