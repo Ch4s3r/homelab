966f69eb9b66774b6874482bfa41c89ba625ee77
Ch4s3r <lamprecht.patrick1@gmail.com>
2024-05-19 17:36:28 +0200

    Try new setup without argocd autopilot

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode=644 --cluster-cidr=10.42.0.0/16,2001:cafe:42::/56 --service-cidr=10.43.0.0/16,2001:cafe:43::/112 --default-local-storage-path /mnt/data
+kubectl config view --raw
+pbpaste | tee ~/.kube/macmini
+set -x KUBECONFIG ~/.kube/macmini
+
+k3d cluster create -i latest
+kubectl annotate storageclass local-path defaultVolumeType=local
+velero install \
+        --use-node-agent \
+        --default-volumes-to-fs-backup \
+        --provider aws \
+        --plugins velero/velero-plugin-for-aws \
+        --bucket velero \
+        --secret-file ./credentials-velero \
+        --use-volume-snapshots=false \
+        --backup-location-config region=minio,s3ForcePathStyle="true",s3Url=http://192.168.178.11:9000
+k apply -f https://raw.githubusercontent.com/vmware-tanzu/velero/main/examples/nginx-app/with-pv.yaml
+k apply -f pod-with-pvc.yaml
+
+k apply -f immich-pvc.yaml
+helm install --namespace immich immich immich/immich -f immich-values.yaml
